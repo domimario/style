5c7fb3dc6d2b988c535a959a05bfdace4a8eae21
Mario Domi <mariodomi@tuta.io>
2023-08-13 23:21:26 +0200

    Font Fixed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import ProfileIcon from "../../assets/eventsImage/Hasbulla.jpg";
 const Navbar = (props) => {
   return (
     <nav className="navbar">
-      <h3>AppName</h3>
+      <h3 className="app-name">AppName</h3>
       <div className="navbar-profile">
-        <h4>John</h4>
-        <img src={ProfileIcon} alt="Profile-Icon" />
+        <h4 className="profile-name"> John</h4>
+        <img src={ProfileIcon} alt="Profile-Icon" className="profile-img" />
       </div>
     </nav>
   );

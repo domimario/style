44dbedaacad32389806b250cb1139167b159a9cf
Mario Domi <mariodomi@tuta.io>
2023-08-14 03:17:18 +0200

    Fixed the backroundImage , and Margins

--- client/src/components/suggestedCard/SuggestedCard.jsx
+++ client/src/components/suggestedCard/SuggestedCard.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./SuggestedCard.css";
 
-const SuggestedCard = ({ date, time, tittle, bgImage }) => {
+const SuggestedCard = ({ date, time, title, bgImage }) => {
   const cardBg = {
     backgroundImage: `url(${bgImage})`,
   };
   return (
     <>
-      <div className="card-content" style={cardBg}>
-        <div>
-          <div className="card-date">
-            {date}-{time}
-          </div>
-          <div className="card-tittle">{tittle}</div>
-          <button className="card-button">Ticket</button>
+      <div className="card">
+        <div className="card-background" style={cardBg}></div>
+        <div className="card-content">
+          <p className="card-date">
+            {date} - {time}
+          </p>
+          <p className="card-title">{title}</p>
+          <button className="card-button">Tickets</button>
         </div>
-      
       </div>
     </>
   );

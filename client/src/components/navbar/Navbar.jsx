e60a5ad8a88f171848fc9b6ce0cc1680d1a2919b
Mario Domi <mariodomi@tuta.io>
2023-08-15 12:16:49 +0200

    Change Text with components

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import "./Navbar.css";
 import ProfileIcon from "../../assets/eventsImage/Hasbulla.jpg";
-
+import Text from "../text/Text";
 const Navbar = (props) => {
   return (
     <nav className="navbar">
-      <h3 className="app-name">AppName</h3>
+      <div className="app-name">
+        <Text
+          text={"AppName"}
+          family={"open-sans"}
+          weight={"bold"}
+          size={"s20"}
+          lineheight={"l27"}
+          color={"white"}
+        />
+      </div>
+
       <div className="navbar-profile">
-        <h4 className="profile-name"> John</h4>
+        <h4 className="profile-name">
+          <Text
+            text={"John"}
+            family={"open-sans"}
+            weight={"regular"}
+            size={"s16"}
+            lineheight={"l22"}
+            color={"white"}
+          />
+        </h4>
+
         <img src={ProfileIcon} alt="Profile-Icon" className="profile-img" />
       </div>
     </nav>

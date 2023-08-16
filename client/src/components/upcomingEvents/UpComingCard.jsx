20c265e0502ff6834da667dda04752e67087c9df
Mario Domi <mariodomi@tuta.io>
2023-08-16 10:29:20 +0200

    change text with component

--- client/src/components/upcomingEvents/UpComingCard.jsx
+++ client/src/components/upcomingEvents/UpComingCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./UpComingCard.css";
 import { useNavigate } from "react-router-dom";
+import Text from "../text/Text";
 const UpComingCard = ({
   date,
   month,
@@ -28,19 +29,71 @@ const UpComingCard = ({
           onClick={handleNavigate}
         ></div>
         <div className="event-date">
-          <p className="date"> {date}</p>
-          <p className="month">{month}</p>
+          <div className="date">
+            <Text
+              text={`${date}`}
+              family={"open-sans"}
+              lineheight={"l13"}
+              size={"s14"}
+              weight={"bold"}
+              color={"white"}
+            />
+          </div>
+          <div className="month">
+            <Text
+              text={`${month}`}
+              family={"open-sans"}
+              lineheight={"l13"}
+              size={"s14"}
+              weight={"regular"}
+              color={"white"}
+            />
+          </div>
         </div>
 
         <div className="event-price">
-          <p className="price">{price}</p>
+          <div className="price">
+            <Text
+              text={`${price}`}
+              family={"open-sans"}
+              lineheight={"l13"}
+              size={"s14"}
+              weight={"bold"}
+              color={"black"}
+            />
+          </div>
         </div>
 
         <div className="event-content">
-          <h4 className="title">{tittle}</h4>
-          <p className="location-time">
-            {location} - {time}
-          </p>
+          <div className="title">
+            <Text
+              text={`${tittle}`}
+              family={"open-sans"}
+              lineheight={"l24"}
+              size={"s18"}
+              weight={"regular"}
+              color={"white"}
+            />
+          </div>
+          <div className="location-time">
+            <Text
+              text={`${location}`}
+              family={"open-sans"}
+              lineheight={"l22"}
+              size={"s16"}
+              weight={"regular"}
+              color={"white"}
+            />{" "}
+            - {""}
+            <Text
+              text={`${time}`}
+              family={"open-sans"}
+              lineheight={"l22"}
+              size={"s16"}
+              weight={"regular"}
+              color={"white"}
+            />
+          </div>
         </div>
       </div>
     </>

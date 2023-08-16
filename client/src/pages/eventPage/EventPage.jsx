eaa653b58f1426ae08c3de1f6fe5c8874e7f7cc6
Mario Domi <mariodomi@tuta.io>
2023-08-16 11:11:03 +0200

    change text with components

--- client/src/pages/eventPage/EventPage.jsx
+++ client/src/pages/eventPage/EventPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./EventPage.css";
-import Text from "../../components/textEvent/Text";
+import TextEvent from "../../components/textEvent/TextEvent";
 import eventBackground from "../../assets/eventsImage/img2.jpg";
 import calendarIcon from "../../assets/icons/calendar.svg";
 import locationIcon from "../../assets/icons/location.svg";
 import ticketIcon from "../../assets/icons/ticket.svg";
+import backImg from "../../assets/icons/Icons+Indicators---System-Icons---chevron_right---#333333.png";
 import { useNavigate } from "react-router-dom";
+import Text from "../../components/text/Text";
 
 const EventPage = (props) => {
   const eventBg = {
@@ -21,58 +23,165 @@ const EventPage = (props) => {
       <div className="event-page-container">
         <div className="page-background" style={eventBg}>
           <div className="page-head">
-            <button className="page-back " onClick={handleNavigate}>
-              <i className="fa-solid fa-chevron-right fa-rotate-180 back-icon"></i>
-            </button>
-            <h4 className="page-title">DAVE</h4>
-          </div>
-
-          <div className="event-page-details">
-            <div className="event-price">
-              <h4 className="page-name">Dave</h4>
-              <p className="page-price">$45.00</p>
+            <div className="page-back " onClick={handleNavigate}>
+              <img src={backImg} alt="" className="back-icon" />
             </div>
-            <div className="page-calendar mb-24">
+
+            <div className="page-title">
+              <Text
+                text={"Dave"}
+                family={"open-sans"}
+                lineheight={"l24"}
+                size={"s18"}
+                weight={"bold"}
+                color={"white"}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="event-page-details">
+          <div className="event-page-price">
+            <div className="pagename">
+              <Text
+                text={"Dave"}
+                family={"open-sans"}
+                lineheight={"l27"}
+                size={"s20"}
+                weight={"bold"}
+                color={"white"}
+              />
+            </div>
+
+            <div className="pageprice">
+              <Text
+                text={"$45.00"}
+                family={"open-sans"}
+                lineheight={"l13"}
+                size={"s16"}
+                weight={"bold"}
+                color={"black"}
+              />
+            </div>
+          </div>
+          <div className="pageweb">
+            <div className="page-specification">
               <div className="bg">
                 <img src={calendarIcon} alt="" />
               </div>
               <div className="date-time">
-                <div>
-                  <p className="date m-0">02</p>
-                  <p className="month m-0">Septembre</p>
+                <div className="roww">
+                  <div className="pagedate">
+                    <Text
+                      text={"02"}
+                      family={"open-sans"}
+                      lineheight={"l22"}
+                      size={"s16"}
+                      weight={"bold"}
+                      color={"white"}
+                    />
+                  </div>
+                  <div className="pagemonth">
+                    <Text
+                      text={"September"}
+                      family={"open-sans"}
+                      lineheight={"l22"}
+                      size={"s16"}
+                      weight={"regular"}
+                      color={"white"}
+                    />
+                  </div>
                 </div>
                 <hr className="spacer" />
-                <div>
-                  <p className="time m-0">10:00 PM</p>
-                  <p className="day m-0">Tuesday</p>
+                <div className="roww">
+                  <div className="pagedate">
+                    <Text
+                      text={"10:00 PM"}
+                      family={"open-sans"}
+                      lineheight={"l22"}
+                      size={"s16"}
+                      weight={"bold"}
+                      color={"white"}
+                    />
+                  </div>
+                  <div className="pageday">
+                    <Text
+                      text={"Tuesday"}
+                      family={"open-sans"}
+                      lineheight={"l22"}
+                      size={"s16"}
+                      weight={"regular"}
+                      color={"white"}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="page-location mb-24">
+            <div className="page-specification">
               <div className="bg">
-                <img src={locationIcon} alt="" className="" />
+                <img src={locationIcon} alt="" />
               </div>
-              <p>Amsterdam, Netherlands</p>
+              <div>
+                <Text
+                  text={"Amsterdam, Netherlands"}
+                  family={"open-sans"}
+                  lineheight={"l22"}
+                  size={"s16"}
+                  weight={"regular"}
+                  color={"white"}
+                />
+              </div>
             </div>
-            <div className="page-ticket mb-24">
+            <div className="page-specification">
               <div className="bg">
                 <img src={ticketIcon} alt="" />
               </div>
-              <p>5 Tickets</p>
+              <div>
+                <Text
+                  text={"5 tickets"}
+                  family={"open-sans"}
+                  lineheight={"l22"}
+                  size={"s16"}
+                  weight={"regular"}
+                  color={"white"}
+                />
+              </div>
             </div>
           </div>
         </div>
 
         <div className="page-about">
-          <h3>About</h3>
-          <Text
-            description={
-              "Lorem ipsum dolor sit amet. Ea nobis corporis ad fugiat culpa ut quibusdam soluta aut aliquid saepe. Est explicabo neque aut molestiae doloribus At fugiat ipsum non cupiditate soluta sed dolorem asperiores aut quia perferendis sed possimus officiis."
-            }
-          />
+          <div>
+            {" "}
+            <div className="page-about-title">
+              <Text
+                text={"About"}
+                family={"open-sans"}
+                lineheight={"l22"}
+                size={"s16"}
+                weight={"bold"}
+                color={"white"}
+              />
+            </div>
+            <TextEvent
+              description={
+                "Lorem ipsum dolor sit amet. Ea nobis corporis ad fugiat culpa ut quibusdam soluta aut aliquid saepe. Est explicabo neque aut molestiae doloribus At fugiat ipsum non cupiditate soluta sed dolorem asperiores aut quia perferendis sed possimus officiis."
+              }
+            />
+          </div>
+
           <button className="order-ticket">
             <img src={ticketIcon} alt="" />
-            Tickets
+            <div>
+              <Text
+                text={"Tickets"}
+                family={"open-sans"}
+                lineheight={"l22"}
+                size={"s16"}
+                weight={"regular"}
+                color={"white"}
+              />
+            </div>
           </button>
         </div>
       </div>

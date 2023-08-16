cabc975eae3f039595a096166740a08185a634d1
Mario Domi <mariodomi@tuta.io>
2023-08-16 10:50:51 +0200

    change text with components

--- client/src/components/upcomingEvents/CategoryEvents.jsx
+++ client/src/components/upcomingEvents/CategoryEvents.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./CategoryEvents.css";
+import Text from "../text/Text";
 const CatergoryEvents = ({ category }) => {
   return (
     <>
-      <button className="category-button">{category}</button>
+      <button className="category-button">
+        <Text
+          text={`${category}`}
+          family={"open-sans"}
+          lineheight={"l22"}
+          size={"s16"}
+          weight={"regular"}
+          color={"gray"}
+        />
+      </button>
     </>
   );
 };
